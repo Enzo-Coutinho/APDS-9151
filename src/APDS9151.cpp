311b5ec83f9595978317a712d10014d4b2cf148b
Enzo-Coutinho <enzo@stemos.com.br>
2025-02-27 15:08:46 -0300

    fix: load

--- src/APDS9151.cpp
+++ src/APDS9151.cpp
@@ -14,6 +14,7 @@ i2c_master_bus_config_t i2c_master_config = {
     .scl_io_num = i2c_scl_port,
     .clk_source = I2C_CLK_SRC_DEFAULT,
     .glitch_ignore_cnt = 7,
+    .intr_priority = 0,
 };
 
 i2c_device_config_t dev_cfg = {
@@ -25,33 +26,87 @@ i2c_device_config_t dev_cfg = {
 i2c_master_dev_handle_t dev_handle;
 
 
-esp_err_t initializeI2CMaster()
+void initalize()
 {
-    return i2c_new_master_bus(&i2c_master_config, &bus_handle);
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_master_config, &bus_handle));
+    ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle));
 }
 
-esp_err_t initializeI2CSlave()
+esp_err_t writeRegister(uint8_t reg, uint8_t data)
 {
-    return i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle);
+    uint8_t buff[2] = {reg, data};
+    return i2c_master_transmit(dev_handle, buff, sizeof(data), 1000 / portTICK_PERIOD_MS);
 }
 
-esp_err_t transmitDataToSlave(const uint8_t * data)
+void readRegister(uint8_t reg, uint8_t len, uint8_t *data)
 {
-    return i2c_master_transmit(dev_handle, data, data_transmit_lenght, -1);
+    ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, &reg, 1, data, len, 1000 / portTICK_PERIOD_MS));
 }
 
-uint8_t * readDataOfSlave()
+uint8_t get_ls_data_green()
 {
-    uint8_t * data_rd = new uint8_t;
-    data_rd = nullptr;
-    ESP_ERROR_CHECK(i2c_master_receive(dev_handle, data_rd, data_receive_lenght, -1));
-    return data_rd;
+    uint8_t data[1];
+    readRegister(registers::__LS_DATA_GREEN_0_ADDR, 1, data);
+    uint8_t greenColor = data[0];
+    return greenColor;
+
+}
+uint8_t get_ls_data_blue()
+{
+    uint8_t data[1];
+    readRegister(registers::__LS_DATA_BLUE_0_ADDR, 1, data);
+    uint8_t blueColor = data[0];
+    return blueColor;
 }
 
-uint8_t * writeAndReadDataOfSlave(const uint8_t * data)
+uint8_t get_ls_data_red()
 {
-    uint8_t buf[20] = {0x0A};
-    uint8_t * buffer = new uint8_t[8];
-    ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, buf, data_receive_lenght, buffer, data_transmit_lenght, -1));
-    return buffer;
+    uint8_t data[1];
+    readRegister(registers::__LS_DATA_RED_0_ADDR, 1, data);
+    uint8_t redColor = data[0];
+    return redColor;
+}
+
+uint8_t get_ir_result()
+{
+    uint8_t data[1];
+    readRegister(registers::__LS_DATA_IR_0_ADDR, 1, data);
+    uint8_t ir = data[0];
+    return ir;
+}
+
+uint8_t get_main_status()
+{
+    uint_least8_t data[1];
+    readRegister(registers::__MAIN_STATUS_ADDR, 1, data);
+    uint_least8_t status = data[0];
+    return status;
+}
+
+uint8_t get_part_id()
+{
+    uint8_t data[1];
+    readRegister(registers::__PART_ID_ADDR, 1, data);
+    uint8_t status = data[0];
+    return status;
+}
+
+uint8_t get_ps_data()
+{
+    uint8_t data;
+    readRegister(registers::__PS_DATA_0_ADDR, 1, & data);
+    uint8_t status = data;
+    return status;
+}
+
+esp_err_t device_is_conected()
+{
+    return i2c_master_probe(bus_handle, dev_cfg.device_address, 1000 / portTICK_PERIOD_MS);
+}
+
+uint8_t get_configure()
+{
+    uint8_t data[1];
+    readRegister(registers::__INT_CFG_ADDR, 1, data);
+    return data[0];
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,17 @@
 #include "APDS9151.h"
 void setup() {
   Serial.begin(115200);
-  ESP_ERROR_CHECK(initializeI2CMaster());
-  ESP_ERROR_CHECK(initializeI2CSlave());
+  initalize();
+  uint8_t initial_configure[2] = {registers::__MAIN_CTRL_ADDR, data_main_ctrl::__RGB_MODE | data_main_ctrl::__LS_EN | data_main_ctrl::__PS_EN | data_main_ctrl::__SW_RESET};
+  ESP_ERROR_CHECK(writeRegister(initial_configure[0], initial_configure[1]));
+  ESP_ERROR_CHECK(writeRegister(registers::__LS_MEAS_RATE_ADDR, __LS_RESOLUTION_17bit | __LS_MEASUREMENT_RATE_25ms));
+  ESP_ERROR_CHECK(writeRegister(__LS_GAIN_ADDR, __GAIN_3));
+  ESP_ERROR_CHECK(writeRegister(registers::__PS_PULSES_ADDR, ps_freq::__FREQ_60K | __PULSE_CURRENT_100MA));
+  ESP_ERROR_CHECK(writeRegister(registers::__PS_PULSES_ADDR, ps_pulses::__PS_PULSES_8));
+  ESP_ERROR_CHECK(writeRegister(registers::__PS_MEAS_RATE_ADDR, resolution_ps::__PS_RESOLUTION_8bit | resolution_ps::__PS_MEASUREMENT_RATE_100ms));
+  ESP_ERROR_CHECK(device_is_conected());
 }
 
 void loop() {
-  uint8_t * data = readDataOfSlave();
-  Serial.println(*(data));
+  Serial.println(get_ls_data_blue());
 }
