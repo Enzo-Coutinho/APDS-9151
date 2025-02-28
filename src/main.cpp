c2c812932ef7ebdcd6218dfdb1eb74a4a02e2996
Enzo-Coutinho <enzo@stemos.com.br>
2025-02-28 08:06:15 -0300

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,11 @@
 void setup() {
   Serial.begin(115200);
   initalize();
-  //uint8_t initial_configure[2] = {registers::__MAIN_CTRL_ADDR, data_main_ctrl::__SW_RESET};
+  uint8_t initial_configure[2] = {registers::__MAIN_CTRL_ADDR, data_main_ctrl::__RGB_MODE};
+  ESP_ERROR_CHECK(writeRegister(initial_configure[0], initial_configure[1]));
   /*
   uint8_t pro_configure[2] = {registers::__MAIN_CTRL_ADDR, data_main_ctrl::__RGB_MODE | data_main_ctrl::__LS_EN | data_main_ctrl::__PS_EN};
-  ESP_ERROR_CHECK(writeRegister(initial_configure[0], initial_configure[1]));
+  
   ESP_ERROR_CHECK(writeRegister(pro_configure[0], pro_configure[1]));
   ESP_ERROR_CHECK(writeRegister(registers::__LS_MEAS_RATE_ADDR, __LS_RESOLUTION_17bit | __LS_MEASUREMENT_RATE_25ms));
   ESP_ERROR_CHECK(writeRegister(__LS_GAIN_ADDR, __GAIN_3));
@@ -15,7 +16,6 @@ void setup() {
   ESP_ERROR_CHECK(writeRegister(registers::__PS_MEAS_RATE_ADDR, resolution_ps::__PS_RESOLUTION_8bit | resolution_ps::__PS_MEASUREMENT_RATE_100ms));
   */
   ESP_ERROR_CHECK(device_is_conected());
-  ESP_ERROR_CHECK(writeRegister(registers::__INT_CFG_ADDR, interrupt::__LS_VAR_MODE));
 }
 
 void loop() {
